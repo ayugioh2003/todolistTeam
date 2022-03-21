1843f51eb105820038a1777ab58afea8a1c9c21e
oneraner <oneraner@gmail.com>
2022-03-22 01:17:12 +0800

    change server

---
+++ controller/todosController.js
@@ -0,0 +1,2 @@
+export const getTodos = () => {};
+export const postTodos = () => {};
---
+++ model/todoModel.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const todos = [
+  { id: uuidv4(), title: "test data 1", content: "mock test data" },
+];
+
+export default todos;
